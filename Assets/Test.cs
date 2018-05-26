b4892897e7a431b60a16321d5aab9cca5a8bc123
daiki-ypshimaru <godeater1104@yahoo.co.jp>
2018-05-26 18:56:15 +0900

    Lesson4の課題提出
    配列を宣言し、出力。

---
+++ Assets/Test.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using System.Collections;
+public class Test : MonoBehaviour {
+    void Start () { 
+    
+        int[] array = {60, 70, 40, 50, 80};
+        for (int i = 4; i >= 0; i--)
+        {
+                Debug.Log (array [i]);
+            }
+
+        }
+
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
